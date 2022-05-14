3550226d2d5bc5adff641333d3506ad9440071d5
Eduardo Venegas <eduardo.venegas@mediaagility.com>
2022-05-13 23:35:38 -0500

    Pausa en las clases del curso para generar archivo apk

--- src/screens/Account.jsx
+++ src/screens/Account.jsx
@@ -2,9 +2,11 @@ import { View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import React from 'react';
 
+import styles from '../styles/styles';
+
 const Account = () => {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.container}>
             <Text>Account</Text>
         </SafeAreaView>
     );
--- src/screens/Pokedex.jsx
+++ src/screens/Pokedex.jsx
@@ -2,9 +2,11 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import styles from '../styles/styles';
+
 const Pokedex = () => {
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.container}>
             <Text>Pokedex</Text>
         </SafeAreaView>
     );
