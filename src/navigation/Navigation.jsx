2ddce7ee7d1f328dfee98ca6a52adfea884a4786
Eduardo Venegas <eduardo.venegas@mediaagility.com>
2022-05-13 20:38:23 -0500

    Clase #3 - Tab Navigation con iconos

--- src/navigation/Navigation.jsx
+++ src/navigation/Navigation.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { Image } from 'react-native';
+
 import Favorite from '../screens/Favorite';
 import Account from '../screens/Account';
 import Pokedex from '../screens/Pokedex';
@@ -9,11 +12,50 @@ const Tab = createBottomTabNavigator();
 const Navigation = () => {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Favorite" component={Favorite}/>
-            <Tab.Screen name="Pokedex" component={Pokedex}/>
-            <Tab.Screen name="Account" component={Account}/>
+            <Tab.Screen name="Favorite"
+                component={Favorite}
+                options={{
+                    tabBarLabel: "Favoritos",
+                    tabBarIcon: ({ color, size }) => {
+                        return (
+                            <Icon name='heart' color={color} size={size} />
+                        )
+                    }
+                }}
+            />
+            <Tab.Screen name="Pokedex"
+                component={Pokedex}
+                options={{
+                    tabBarLabel: "",
+                    tabBarIcon: () => {
+                        return (
+                            renderPokeball()
+                        )
+                    }
+                }}
+            />
+            <Tab.Screen name="Account"
+                component={Account}
+                options={{
+                    tabBarLabel: "Mi cuenta",
+                    tabBarIcon: ({ color, size }) => {
+                        return (
+                            <Icon name='user' color={color} size={size} />
+                        )
+                    }
+                }}
+            />
         </Tab.Navigator>
     );
 };
+
+function renderPokeball() {
+    return (
+        <Image
+            source={require('../assets/pokeball.png')}
+            style={{ width: 75, height: 75, top: -15 }}
+        />
+    );
+}
 
 export default Navigation;
