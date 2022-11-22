4e25a31e7eafb6a0e3cdc5d703890a7706d92e9b
Eduardo Venegas <eduardo.venegas@mediaagility.com>
2022-11-21 19:48:32 -0600

    Clase #4 - Formik y Yup

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -7,26 +7,55 @@ import {
     Button,
     Keyboard,
 } from 'react-native';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const LoginForm = () => {
+    
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            password: ''
+        },
+        validationSchema: Yup.object({
+            username: Yup.string().required('El usuario es obligatorio'),
+            password: Yup.string().required('La contraseña es obligatoria'),
+        }),
+        validateOnChange: false, // La validación solo se ejecutará al enviar el formulario...
+        onSubmit: (values) => {
+            console.log('Formulario enviado...');
+            console.log(values);
+        },
+    });
+
     return (
         <View>
             <Text style={ styles.title }>Iniciar sesión</Text>
             <TextInput 
                 placeholder='Nombre de usuario'
-                style={styles.input}
+                style={ styles.input }
                 autoCapitalize='none'
+                value={ formik.values.username }
+                onChangeText={
+                    (text) => formik.setFieldValue('username', text)
+                }
             />
             <TextInput
                 placeholder='Contraseña'
-                style={styles.input}
+                style={ styles.input }
                 autoCapitalize='none'
-                secureTextEntry={true}
+                secureTextEntry={ true }
+                value={ formik.values.password }
+                onChangeText={
+                    (text) => formik.setFieldValue('password', text)
+                }
             />
             <Button 
                 title='Entrar'
-                onPress={() => console.log('Entrando...')}
+                onPress={ formik.handleSubmit }
             />
+            <Text style={ styles.error }>{ formik.errors.username }</Text>
+            <Text style={ styles.error }>{ formik.errors.password }</Text>
         </View>
     );
 }
@@ -46,6 +75,11 @@ const styles = StyleSheet.create({
         padding: 10,
         borderRadius: 10,
     },
+    error: {
+        textAlign: 'center',
+        color: '#f00',
+        marginTop: 20
+    }
 });
 
 export default LoginForm;
